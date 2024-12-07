43bf1c0f8533a2274903cfaf2ce6f5d127f46ced
José Iêdo <iedoduarte1@gmail.com>
2024-12-07 19:42:59 -0300

    create initial version of the script to check if the correct windows are near

--- enigma_window.gd
+++ enigma_window.gd
@@ -4,30 +4,74 @@ class_name EnigmaWindow
 
 @onready var node_2d: Node2D = $".."
 
+# Windows that must be around
+var right_window: Window = null
+var bottom_window: Window = null
+var left_window: Window = null
+var top_window: Window = null
+
 func _on_ready():
 	focus_entered.connect(_test)
 	print("test")
 
-func init(initialX, initialY):
-	position.x = initialX
-	position.y = initialY
-	size = Vector2i(278, 316)
+func init(initial_x, initial_y):
+	self.position.x = initial_x
+	self.position.y = initial_y
+	self.size = Vector2i(278, 316)
+	return self
+
+func setup_neighbors(top_window, right_window, bottom_window, left_window):
+	self.top_window = top_window
+	self.right_window = right_window
+	self.bottom_window = bottom_window
+	self.left_window = left_window
 	return self
 
 func _test() -> void:
 	print("test")
 
+var done = false
 func _process(delta: float) -> void:
 	if (not has_focus()):
 		return
-	
-	print(name, " position.x", position.x)
-	print(name, " position.y", position.y)
-	
-		
-#func _compare_measures(expected, current):
-	#var tolerance = 10
-	#if current > expected - tolerance && current < expected + tolerance:
-		#return true
-	#else:
-		#return false
+	if (done):
+		print("uga")
+	if top_window != null:
+		var distanceY = abs(position.y - top_window.position.y - size.y)
+		var distanceX = abs(position.x - top_window.position.x)
+		print("TOP ----------------------------------------------------------------")
+		print("distanceX", distanceX, "curr", position, "right", top_window.position)
+		print("distanceY", distanceY, "curr", position, "right", top_window.position)
+		done = distanceY <= 15 && distanceX <= 2
+	if right_window != null:
+		var distanceY = abs(position.y - right_window.position.y)
+		var distanceX = abs(position.x - right_window.position.x + size.x)
+		print("RIGHT ----------------------------------------------------------------")
+		print("distanceX", distanceX, "curr", position, "right", right_window.position)
+		print("distanceY", distanceY, "curr", position, "right", right_window.position)
+		done = distanceY <= 2 && distanceX <= 2
+	if bottom_window != null:
+		var distanceY = abs(position.y - bottom_window.position.y + size.y)
+		var distanceX = abs(position.x - bottom_window.position.x)
+		print("BOTTOM ----------------------------------------------------------------")
+		print("distanceX ", distanceX, " curr ", position, " bottom ", bottom_window.position)
+		print("distanceY ", distanceY, " curr ", position, " bottom ", bottom_window.position)
+		done = distanceY <= 15 && distanceX <= 2
+	if left_window != null:
+		var distanceY = abs(position.y - left_window.position.y)
+		var distanceX = abs(position.x - left_window.position.x - size.x)
+		print("LEFT ----------------------------------------------------------------")
+		print("distanceX", distanceX, "curr", position, "left", left_window.position)
+		print("distanceY", distanceY, "curr", position, "left", left_window.position)
+		done = distanceY <= 2 && distanceX <= 2
+
+
+func calculate_distance_and_check(window, offset_x, offset_y, threshold_x, threshold_y):
+	if window == null:
+		return null
+	var distanceY = abs(position.y - window.position.y + offset_y)
+	var distanceX = abs(position.x - window.position.x + offset_x)
+	print(window.name.upper() + "---------------------------------------------------------")
+	print("distanceX: ", distanceX, "curr: ", position, " window:", window.position)
+	print("distanceY: ", distanceY, "curr: ", position, " window:", window.position)
+	return distanceY <= threshold_y and distanceX <= threshold_x
--- node_2d.gd
+++ node_2d.gd
@@ -9,20 +9,30 @@ func _ready() -> void:
 	window.mode = Window.MODE_MINIMIZED
 	window.transparent = false
 
-	# Top
-	var lt = _create_window(193, 147)
-	var mt = _create_window(600, 550)
-	var rt = _create_window(800, 450)
-	
-	# Mid
-	var lm = _create_window(200, 160)
-	var mm = _create_window(500, 250)
-	var rm = _create_window(230, 950)
-	
-	# Bottom
-	var lb = _create_window(783, 147)
-	var mb = _create_window(650, 550)
-	var rb = _create_window(120, 550)
+	var top_middle = _create_window(193, 200)
+	var top_left = _create_window(193, 147)
+	#var top_right = _create_window(800, 450)
+
+	#var middle_left = _create_window(200, 160)
+	#var middle_middle = _create_window(500, 250)
+	#var middle_right = _create_window(230, 950)
+
+	#var bottom_left = _create_window(783, 147)
+	#var bottom_middle = _create_window(650, 550)
+	#var bottom_right = _create_window(120, 550)
+	top_left.setup_neighbors(null, null, null, top_middle)
+	#top_middle.setup_neighbors(null, top_right, middle_middle, top_left)
+	# top_middle.setup_neighbors(top_left, null,null, null)
+	#top_right.setup_neighbors(null, null, middle_right, top_middle)
+
+	#middle_left.setup_neighbors(top_left, middle_middle, bottom_left, null)
+	#middle_left.setup_neighbors(top_left, null, null, null)
+	#middle_middle.setup_neighbors(top_middle, middle_right, bottom_middle, middle_left)
+	#middle_right.setup_neighbors(top_right, null, bottom_middle, middle_middle)
+
+	#bottom_left.setup_neighbors(middle_left, bottom_middle, null, null)
+	#bottom_middle.setup_neighbors(middle_middle, bottom_right, null, bottom_left)
+	#bottom_right.setup_neighbors(middle_right, null, null, bottom_middle)
 
 func _create_window(initialX, initialY) -> Window:
 	var window = Window.new()
@@ -31,8 +41,6 @@ func _create_window(initialX, initialY) -> Window:
 	window.close_requested.connect(_close_game)
 	add_child(window)
 	return window
-	
+
 func _close_game():
 	get_tree().quit()
-func _on_focus_entered():
-	print("Teste")
