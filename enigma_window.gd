953a83bd434d6dfae5053009c0e10963a4c531d3
José Iêdo <iedoduarte1@gmail.com>
2024-12-07 23:58:38 -0300

    add endgame window and close pieces windows automatically, refactor some of the logic of the windows creations

--- enigma_window.gd
+++ enigma_window.gd
@@ -3,7 +3,6 @@ extends Window
 class_name EnigmaWindow
 
 @onready var node_2d: Node2D = $".."
-@onready var texture_rect: TextureRect = $TextureRect
 
 # Windows that must be around
 var right_window: Window = null
@@ -11,15 +10,9 @@ var bottom_window: Window = null
 var left_window: Window = null
 var top_window: Window = null
 
-func _on_ready():
-	focus_entered.connect(_test)
-	print("test")
-
-func init(initial_x, initial_y):
-	self.position.x = initial_x
-	self.position.y = initial_y
+func init(position):
+	self.position = position
 	self.size = Vector2i(278, 316)
-	self.add_child(texture_rect)
 	return self
 
 func setup_neighbors(top_window, right_window, bottom_window, left_window):
@@ -29,52 +22,41 @@ func setup_neighbors(top_window, right_window, bottom_window, left_window):
 	self.left_window = left_window
 	return self
 
-func _test() -> void:
-	print("test")
-
 var piece_fitted = false
 var change_image = false
 var add = false
 func _process(delta: float) -> void:
 	if not has_focus():
-		return
-	print("CURRENT ", name, " ------------------------------")
+		pass
+	
+	validate_window_position()
+	_handle_success_icon()
 	
 	if piece_fitted && add == false:
+		node_2d.window_in_correct_position.emit()
+		
+func _handle_success_icon():
+	var child_count = self.get_child_count()
+	if piece_fitted == false && child_count > 0:
+		var icon = self.get_child(0)
+		self.remove_child(icon)
+	elif piece_fitted == true && child_count == 0:
 		var rect = TextureRect.new()
 		rect.texture = load("res://icon.svg")
 		self.add_child(rect)	
-		var children = get_child_count()
-		print("children: ", children)
-		add = true
-	elif piece_fitted == false:
-		var node =  self.get_child(0)
-		self.remove_child(node)
-		add = false
-	if not has_focus():
-		return
+		
 
-	if piece_fitted:
-		print("all pieces are near!")
-	else:
-		print("away..")
-	var is_top_close = calculate_distance_and_check(top_window, 0, -size.y, 4, 15)
-	var is_right_close = calculate_distance_and_check(right_window, size.x, 0, 15, 4)
-	var is_bottom_close = calculate_distance_and_check(bottom_window, 0, size.y, 4, 15)
-	var is_left_close = calculate_distance_and_check(left_window, -size.x, 0, 15, 4)
-	#piece_fitted=is_left_close
+func validate_window_position():
+	var is_top_close = _calculate_distance_and_check(top_window, 0, -size.y, 4, 15)
+	var is_right_close = _calculate_distance_and_check(right_window, size.x, 0, 15, 4)
+	var is_bottom_close = _calculate_distance_and_check(bottom_window, 0, size.y, 4, 15)
+	var is_left_close = _calculate_distance_and_check(left_window, -size.x, 0, 15, 4)
 	piece_fitted = is_top_close && is_right_close && is_bottom_close && is_left_close
-	#if piece_fitted == false:
-		#remove_child(texture_rect)
-	#if piece_fitted == true && get_children().size() == 0:
-		#add_child(texture_rect)
+	return piece_fitted
 
-func calculate_distance_and_check(window, offset_x, offset_y, threshold_x, threshold_y):
+func _calculate_distance_and_check(window, offset_x, offset_y, threshold_x, threshold_y):
 	if window == null:
 		return true
 	var distanceY = abs(position.y - window.position.y + offset_y)
 	var distanceX = abs(position.x - window.position.x + offset_x)
-	print(window.name + "---------------------------------------------------------")
-	print("distanceX: ", distanceX, "curr: ", position, " window:", window.position)
-	print("distanceY: ", distanceY, "curr: ", position, " window:", window.position)
 	return distanceY <= threshold_y and distanceX <= threshold_x
--- node_2d.gd
+++ node_2d.gd
@@ -3,45 +3,77 @@ extends Node2D
 signal window_in_correct_position
 
 const EnigmaWindow = preload("res://enigma_window.gd")
+@onready var end_game_window: Window = $EndGameWindow
+
+
+var windows = {
+	"top_middle": {"pos": Vector2(193, 200), "neighbors": [null, "top_right", "middle_middle", "top_left"]},
+	"top_left": {"pos": Vector2(193, 147), "neighbors": [null, "top_middle", "middle_left", null]},
+	"top_right": {"pos": Vector2(800, 450), "neighbors": [null, null, "middle_right", "top_middle"]},
+
+	"middle_left": {"pos": Vector2(200, 160), "neighbors": ["top_left", "middle_middle", "bottom_left", null]},
+	"middle_middle": {"pos": Vector2(900, 300), "neighbors": ["top_middle", "middle_right", "bottom_middle", "middle_left"]},
+	"middle_right": {"pos": Vector2(230, 950), "neighbors": ["top_right", null, "bottom_right", "middle_middle"]},
+
+	"bottom_left": {"pos": Vector2(783, 147), "neighbors": ["middle_left", "bottom_middle", null, null]},
+	"bottom_middle": {"pos": Vector2(650, 550), "neighbors": ["middle_middle", "bottom_right", null, "bottom_left"]},
+	"bottom_right": {"pos": Vector2(120, 550), "neighbors": ["middle_right", null, null, "bottom_middle"]}
+}
+
+var windows_nodes = {}
 
 func _ready() -> void:
-	var window = get_viewport().get_window()
-	window.mode = Window.MODE_MINIMIZED
-	window.transparent = false
+	var main_window = get_viewport().get_window()
+	main_window.mode = Window.MODE_MINIMIZED
+	main_window.size = Vector2i(834, 948)
 
-	var top_middle = _create_window(193, 200,"top_middle")
-	var top_left = _create_window(193, 147,"top_left")
-	var top_right = _create_window(800, 450,"top_right")
+	for window_name in windows.keys():
+		var window = windows[window_name]
+		windows_nodes[window_name] = _create_window(window.pos, window_name)
 
-	var middle_left = _create_window(200, 160,"middle_left")
-	var middle_middle = _create_window(500, 250,"middle_middle")
-	var middle_right = _create_window(230, 950,"middle_right")
+	for window_name in windows_nodes.keys():
+		var neighbors = windows[window_name].neighbors
+		windows_nodes[window_name].setup_neighbors(
+			windows_nodes[neighbors[0]] if neighbors[0] else null,
+			windows_nodes[neighbors[1]] if neighbors[1] else null,
+			windows_nodes[neighbors[2]] if neighbors[2] else null,
+			windows_nodes[neighbors[3]] if neighbors[3] else null,
+		)
 
-	var bottom_left = _create_window(783, 147,"bottom_left")
-	var bottom_middle = _create_window(650, 550,"bottom_middle")
-	var bottom_right = _create_window(120, 550,"bottom_right")
 
-	top_left.setup_neighbors(null, top_middle, middle_left, null)
-	top_middle.setup_neighbors(null, top_right, middle_middle, top_left)
-	top_right.setup_neighbors(null, null, middle_right, top_middle)
-
-	middle_left.setup_neighbors(top_left, middle_middle, bottom_left, null)
-	middle_middle.setup_neighbors(top_middle, middle_right, bottom_middle, middle_left)
-	middle_right.setup_neighbors(top_right, null, bottom_right, middle_middle)
-
-	bottom_left.setup_neighbors(middle_left, bottom_middle, null, null)
-	bottom_middle.setup_neighbors(middle_middle, bottom_right, null, bottom_left)
-	bottom_right.setup_neighbors(middle_right, null, null, bottom_middle)
-
-func _create_window(initialX, initialY, name) -> Window:
+func _create_window(window_position, window_name) -> Window:
 	var window = Window.new()
-	window.name = name
-	window.title = name
+	window.name = window_name
+	window.title = window_name
 	window.set_script(EnigmaWindow)
-	window.init(initialX, initialY)
+	window.init(window_position)
 	window.close_requested.connect(_close_game)
 	add_child(window)
 	return window
 
 func _close_game():
 	get_tree().quit()
+
+func _on_window_in_correct_position():
+	var children = get_children()
+	var valid = false
+	for child in children:
+		if child is EnigmaWindow:
+			if not child.piece_fitted:
+				return
+			valid = true
+
+	if valid:
+		var main_window = get_viewport().get_window()
+		main_window.visible = true
+		main_window.popup()
+
+		DisplayServer.window_request_attention(main_window.get_window_id())
+		var middle = windows_nodes["middle_middle"]
+		main_window.title = "Finished!!!!!!!"
+		main_window.position.x = middle.position.x - middle.size.x
+		main_window.position.y = middle.position.y - middle.size.y
+
+		main_window.grab_focus()
+		for child in children:
+			child.queue_free()
