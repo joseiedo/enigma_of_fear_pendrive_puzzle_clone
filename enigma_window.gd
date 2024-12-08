d8651087d6b148f62ee38d7783c5567418a89c22
José Iêdo <iedoduarte1@gmail.com>
2024-12-07 21:15:37 -0300

    add temporary icon and window title/names to debug easily

--- enigma_window.gd
+++ enigma_window.gd
@@ -3,6 +3,7 @@ extends Window
 class_name EnigmaWindow
 
 @onready var node_2d: Node2D = $".."
+@onready var texture_rect: TextureRect = $TextureRect
 
 # Windows that must be around
 var right_window: Window = null
@@ -18,6 +19,7 @@ func init(initial_x, initial_y):
 	self.position.x = initial_x
 	self.position.y = initial_y
 	self.size = Vector2i(278, 316)
+	self.add_child(texture_rect)
 	return self
 
 func setup_neighbors(top_window, right_window, bottom_window, left_window):
@@ -31,18 +33,41 @@ func _test() -> void:
 	print("test")
 
 var piece_fitted = false
+var change_image = false
+var add = false
 func _process(delta: float) -> void:
+	if not has_focus():
+		return
+	print("CURRENT ", name, " ------------------------------")
+	
+	if piece_fitted && add == false:
+		var rect = TextureRect.new()
+		rect.texture = load("res://icon.svg")
+		self.add_child(rect)	
+		var children = get_child_count()
+		print("children: ", children)
+		add = true
+	elif piece_fitted == false:
+		var node =  self.get_child(0)
+		self.remove_child(node)
+		add = false
 	if not has_focus():
 		return
 
 	if piece_fitted:
 		print("all pieces are near!")
-	var is_top_close = calculate_distance_and_check(top_window, 0, -size.y, 2, 15)
-	var is_right_close = calculate_distance_and_check(right_window, size.x, 0, 15, 2)
-	var is_bottom_close = calculate_distance_and_check(bottom_window, 0, size.y, 2, 15)
-	var is_left_close = calculate_distance_and_check(left_window, -size.x, 0, 15, 2)
-
+	else:
+		print("away..")
+	var is_top_close = calculate_distance_and_check(top_window, 0, -size.y, 4, 15)
+	var is_right_close = calculate_distance_and_check(right_window, size.x, 0, 15, 4)
+	var is_bottom_close = calculate_distance_and_check(bottom_window, 0, size.y, 4, 15)
+	var is_left_close = calculate_distance_and_check(left_window, -size.x, 0, 15, 4)
+	#piece_fitted=is_left_close
 	piece_fitted = is_top_close && is_right_close && is_bottom_close && is_left_close
+	#if piece_fitted == false:
+		#remove_child(texture_rect)
+	#if piece_fitted == true && get_children().size() == 0:
+		#add_child(texture_rect)
 
 func calculate_distance_and_check(window, offset_x, offset_y, threshold_x, threshold_y):
 	if window == null:
--- node_2d.gd
+++ node_2d.gd
@@ -9,33 +9,34 @@ func _ready() -> void:
 	window.mode = Window.MODE_MINIMIZED
 	window.transparent = false
 
-	var top_middle = _create_window(193, 200)
-	var top_left = _create_window(193, 147)
-	#var top_right = _create_window(800, 450)
+	var top_middle = _create_window(193, 200,"top_middle")
+	var top_left = _create_window(193, 147,"top_left")
+	var top_right = _create_window(800, 450,"top_right")
 
-	#var middle_left = _create_window(200, 160)
-	#var middle_middle = _create_window(500, 250)
-	#var middle_right = _create_window(230, 950)
+	var middle_left = _create_window(200, 160,"middle_left")
+	var middle_middle = _create_window(500, 250,"middle_middle")
+	var middle_right = _create_window(230, 950,"middle_right")
 
-	#var bottom_left = _create_window(783, 147)
-	#var bottom_middle = _create_window(650, 550)
-	#var bottom_right = _create_window(120, 550)
-	top_left.setup_neighbors(null, null, null, top_middle)
-	#top_middle.setup_neighbors(null, top_right, middle_middle, top_left)
-	# top_middle.setup_neighbors(top_left, null,null, null)
-	#top_right.setup_neighbors(null, null, middle_right, top_middle)
+	var bottom_left = _create_window(783, 147,"bottom_left")
+	var bottom_middle = _create_window(650, 550,"bottom_middle")
+	var bottom_right = _create_window(120, 550,"bottom_right")
 
-	#middle_left.setup_neighbors(top_left, middle_middle, bottom_left, null)
-	#middle_left.setup_neighbors(top_left, null, null, null)
-	#middle_middle.setup_neighbors(top_middle, middle_right, bottom_middle, middle_left)
-	#middle_right.setup_neighbors(top_right, null, bottom_middle, middle_middle)
+	top_left.setup_neighbors(null, top_middle, middle_left, null)
+	top_middle.setup_neighbors(null, top_right, middle_middle, top_left)
+	top_right.setup_neighbors(null, null, middle_right, top_middle)
 
-	#bottom_left.setup_neighbors(middle_left, bottom_middle, null, null)
-	#bottom_middle.setup_neighbors(middle_middle, bottom_right, null, bottom_left)
-	#bottom_right.setup_neighbors(middle_right, null, null, bottom_middle)
+	middle_left.setup_neighbors(top_left, middle_middle, bottom_left, null)
+	middle_middle.setup_neighbors(top_middle, middle_right, bottom_middle, middle_left)
+	middle_right.setup_neighbors(top_right, null, bottom_right, middle_middle)
 
-func _create_window(initialX, initialY) -> Window:
+	bottom_left.setup_neighbors(middle_left, bottom_middle, null, null)
+	bottom_middle.setup_neighbors(middle_middle, bottom_right, null, bottom_left)
+	bottom_right.setup_neighbors(middle_right, null, null, bottom_middle)
+
+func _create_window(initialX, initialY, name) -> Window:
 	var window = Window.new()
+	window.name = name
+	window.title = name
 	window.set_script(EnigmaWindow)
 	window.init(initialX, initialY)
 	window.close_requested.connect(_close_game)
